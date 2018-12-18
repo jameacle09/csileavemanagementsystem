cb40599acf3d41e1861af0a96c84b236d7dc1ec9
Shahrul Ridzuan Aliyas <shahrul91@gmail.com>
2018-12-18 14:56:32 +0800

    Add base source code

---
+++ src/main/java/com/csi/leavemanagement/LeavemanagementApplication.java
@@ -0,0 +1,14 @@
+package com.csi.leavemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeavemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeavemanagementApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/csi/leavemanagement/LeavemanagementApplicationTests.java
@@ -0,0 +1,17 @@
+package com.csi.leavemanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LeavemanagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
