ac402d024dee8b735bd9c16ba80f66ad6b608121
Shahrul Ridzuan Aliyas <shahrul91@gmail.com>
2018-12-20 17:55:59 +0800

    Update front-end code

---
+++ leavemanagement/src/common/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+      return (
+        <div>
+          <p>Footer</p>
+        </div>
+      );
+    }
+  }
+  
+export default Footer;
