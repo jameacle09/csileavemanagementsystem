58d175c53e0ee3a0906daf2900fc1c21c76ce18e
Shahrul Ridzuan Aliyas <shahrul91@gmail.com>
2019-01-11 10:48:28 +0800

    Update My Leave History page

--- leavemanagement/src/leaves/MyLeaveHistory.js
+++ leavemanagement/src/leaves/MyLeaveHistory.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Table, Button } from 'reactstrap';
+import { Link } from "react-router-dom";
 
 class MyLeaveHistory extends Component {
   constructor(props) {
@@ -6,8 +8,35 @@ class MyLeaveHistory extends Component {
   }
   render() {
     return (
-      <div>
-        <h1>My Leave History</h1>
+      <div className="container">
+        <Table bordered responsive>
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Start Date</th>
+              <th>End Date</th>
+              <th>Duration</th>
+              <th>Leave Type</th>
+              <th>Reason</th>
+              <th>Applied Date</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+            </tr>
+          </tbody>
+        </Table>
       </div>
     );
   }
