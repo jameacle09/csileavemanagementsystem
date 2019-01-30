e41bd0f293a525d089638305d3f9aeb9634a2725
Shahrul Ridzuan Aliyas <shahrul91@gmail.com>
2019-01-30 18:09:22 +0800

    WIP, call rest api in liststaffprofile table

--- Client_CSILMS/src/staffprofile/StaffTableRow.js
+++ Client_CSILMS/src/staffprofile/StaffTableRow.js
@@ -6,21 +6,21 @@ class StaffTableRow extends Component {
   render() {
     return (
       <tr>
-        <td>{this.props.staffprofile.csiStaffId}</td>
-        <td>{this.props.staffprofile.staffName}</td>
-        <td>{this.props.staffprofile.email}</td>
-        <td>{this.props.staffprofile.icNumber}</td>
+        <td>{this.props.staffprofile.emplId}</td>
+        <td>{this.props.staffprofile.name}</td>
+        <td>{this.props.staffprofile.businessEmail}</td>
+        <td>{this.props.staffprofile.nricPassport}</td>
         <td>{this.props.staffprofile.jobTitle}</td>
         <td>{this.props.staffprofile.mobileNo}</td>
         <td>{this.props.staffprofile.businessUnit}</td>
-        <td>{this.props.staffprofile.lineManagerId}</td>
+        <td>{this.props.staffprofile.managerName}</td>
         <td>{this.props.staffprofile.joinDate}</td>
         <td>
           <Button
             className="btn btn-primary"
             color="primary"
             tag={Link}
-            to={`/liststaffprofile/edit/${this.props.staffprofile.csiStaffId}`}
+            to={`/liststaffprofile/edit/${this.props.staffprofile.emplId}`}
             activeclassname="active"
           >
             <span className="fa fa-edit" />
