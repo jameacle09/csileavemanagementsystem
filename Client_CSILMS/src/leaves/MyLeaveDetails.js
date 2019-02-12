63f05488b4a2175275c747ea14519d19be197d32
Shahrul Ridzuan Aliyas <shahrul91@gmail.com>
2019-02-12 15:53:04 +0800

    WIP, updated My Leave Details

--- Client_CSILMS/src/leaves/MyLeaveDetails.js
+++ Client_CSILMS/src/leaves/MyLeaveDetails.js
@@ -1,8 +1,64 @@
 import React, { Component } from "react";
 import { Table } from "reactstrap";
+import { fetchData } from '../util/APIUtils';
+import { API_BASE_URL } from '../constants';
+import { withRouter } from 'react-router-dom';
 
 class MyLeaveDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userData: {
+        id: "",
+        leaveCategory: "",
+        carryForward: "",
+        entitlement: "",
+        availableLeave: "",
+        takenLeave: "",
+        balanceLeave: ""
+      }
+    };
+
+    this.loadMyLeaveDetails = this.loadMyLeaveDetails.bind(this);
+
+  }
+
+  loadMyLeaveDetails() {
+    fetchData({
+      url: API_BASE_URL + "/leaveentitlement/me",
+      method: 'GET'
+    }).then(response => {
+      this.setState({
+        userData: response
+      });
+    }).catch(error => {
+      if (error.status === 401) {
+        this.props.history.push("/login");
+      }
+      let userData = {
+        id: "",
+        leaveCategory: "",
+        carryForward: "",
+        entitlement: "",
+        availableLeave: "",
+        takenLeave: "",
+        balanceLeave: ""
+      };
+      this.setState({ userData: userData });
+    });
+  }
+
+  componentDidMount() {
+    this.loadMyLeaveDetails();
+  }
+
+  componentDidUpdate(nextProps) {
+    if (this.props.isAuthenticated !== nextProps.isAuthenticated) {
+      this.loadMyLeaveDetails();
+    }
+  }
   render() {
+    let userData = this.state.userData;
     return (
       <div className="mainContainerLeavePages">
         <div className="headerContainerFlex">
@@ -26,7 +82,7 @@ class MyLeaveDetails extends Component {
             </thead>
             <tbody>
               <tr>
-                <td>-</td>
+                <td>{userData["leaveCategory"]}</td>
                 <td>-</td>
                 <td>-</td>
                 <td>-</td>
