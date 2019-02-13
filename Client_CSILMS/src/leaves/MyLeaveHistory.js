8f0887755cd50b71dd6cd847a3c304d64cf5d08c
flindralin <cherry.marvel@gmail.com>
2019-02-13 17:04:41 +0800

    wip

--- Client_CSILMS/src/leaves/MyLeaveHistory.js
+++ Client_CSILMS/src/leaves/MyLeaveHistory.js
@@ -1,7 +1,53 @@
 import React, { Component } from "react";
 import { Table } from "reactstrap";
+import { fetchData } from '../util/APIUtils';
+import { API_BASE_URL } from '../constants';
 
 class MyLeaveHistory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userData: []
+    };
+    this.loadMyLeaveHistory = this.loadMyLeaveHistory.bind(this);
+    this.autoIncrementRow = this.autoIncrementRow.bind(this);
+  }
+
+  loadMyLeaveHistory() {
+    fetchData({
+      url: API_BASE_URL + "/appliedleave/E000000001",
+      method: 'GET'
+    }).then(response => {
+      this.setState({
+        userData: response
+      });
+    }).catch(error => {
+      if (error.status === 401) {
+        this.props.history.push("/login");
+      }
+      let userData = [];
+      this.setState({ userData: userData });
+    });
+  }
+
+  componentDidMount() {
+    this.loadMyLeaveHistory();
+  }
+
+  componentDidUpdate(nextProps) {
+    if (this.props.isAuthenticated !== nextProps.isAuthenticated) {
+      this.loadMyLeaveHistory();
+    }
+  }
+
+  autoIncrementRow() {
+    let userData = [];
+    console.log(userData);
+    for (userData = 0; userData < userData.length; userData++) {
+      userData += + userData;
+    }
+  }
+
   render() {
     return (
       <div className="mainContainerLeavePages">
@@ -27,17 +73,23 @@ class MyLeaveHistory extends Component {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-              </tr>
+            {
+                this.state.userData.map(function (item, key) {
+                  return (
+                    <tr key={key}>
+                      <td></td>
+                      <td>{item.startDate}</td>
+                      <td>{item.endDate}</td>
+                      <td>{item.leaveDuration}</td>
+                      <td>{item.leaveDescr}</td>
+                      <td>{item.reason}</td>
+                      <td>{item.effDate}</td>
+                      <td>{item.leaveStatus}</td>
+                      <td></td>
+                    </tr>
+                  )
+                })
+              }
             </tbody>
           </Table>
         </div>
