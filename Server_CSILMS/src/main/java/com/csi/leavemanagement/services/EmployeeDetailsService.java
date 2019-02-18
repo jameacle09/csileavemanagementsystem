cee5689ada5dde1b013688e8027baa77afbbce30
ChaiKP <kinnpong.chai@gmail.com>
2019-02-18 17:36:31 +0800

    New API to lookup employee without login

--- Server_CSILMS/src/main/java/com/csi/leavemanagement/services/EmployeeDetailsService.java
+++ Server_CSILMS/src/main/java/com/csi/leavemanagement/services/EmployeeDetailsService.java
@@ -1,13 +1,16 @@
 package com.csi.leavemanagement.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.csi.leavemanagement.models.EmployeeDetails;
+import com.csi.leavemanagement.models.LoginDetails;
 import com.csi.leavemanagement.models.PersonDetails;
 import com.csi.leavemanagement.repositories.EmployeeDetailsRepository;
+import com.csi.leavemanagement.repositories.LoginDetailsRepository;
 import com.csi.leavemanagement.repositories.PersonDetailsRepository;
 
 @Service
@@ -15,12 +18,15 @@ public class EmployeeDetailsService {
 
 	private EmployeeDetailsRepository employeeDetailRepository;
 	private PersonDetailsRepository personDetailRepository;
+	private LoginDetailsRepository loginDetailRepository;
 
 	@Autowired
 	public EmployeeDetailsService(EmployeeDetailsRepository employeeDetailRepository,
-									PersonDetailsRepository personDetailRepository) {
+									PersonDetailsRepository personDetailRepository,
+									LoginDetailsRepository loginDetailRepository) {
 		this.employeeDetailRepository = employeeDetailRepository;
 		this.personDetailRepository = personDetailRepository;
+		this.loginDetailRepository = loginDetailRepository;
 	}
 
 	public List<EmployeeDetails> findAll() {
@@ -46,5 +52,16 @@ public class EmployeeDetailsService {
 	public List<EmployeeDetails> findAllManagers() {
 		List<EmployeeDetails> managerList = this.employeeDetailRepository.findAllManagers();
 		return managerList;
+	}
+	
+	public List<EmployeeDetails> findEmplidWithoutLogin() {
+		List<LoginDetails> loginList = this.loginDetailRepository.findAll();
+		List<String> emplidList = new ArrayList<String> ();
+		
+		for(LoginDetails loginDetail : loginList) 
+			emplidList.add(loginDetail.getEmplId());
+		
+		List<EmployeeDetails> employeeList = this.employeeDetailRepository.findByEmplIdNotIn(emplidList);
+		return employeeList;
 	}
 }
