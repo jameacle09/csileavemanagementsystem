4d1f0467375e2838b330f3130b4218d63349ed9a
Shahrul Ridzuan Aliyas <shahrul91@gmail.com>
2019-02-18 12:01:25 +0800

    Use react-table in Staff Leave History

--- Client_CSILMS/src/manager/StaffLeaveHistory.js
+++ Client_CSILMS/src/manager/StaffLeaveHistory.js
@@ -3,18 +3,24 @@ import { Table } from "reactstrap";
 // import ManagerSideBar from "./ManagerSideBar";
 import "../common/Styles.css";
 import { Redirect, withRouter } from "react-router-dom";
-import { isManagerRole } from "../util/APIUtils";
-import { fetchData } from "../util/APIUtils";
 import { API_BASE_URL } from "../constants";
 import Moment from "react-moment";
 // import 'moment-timezone';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import {
+  fetchData,
+  isManagerRole,
+  formatDateYMD,
+  formatDateDMY
+} from "../util/APIUtils";
 
 class StaffLeaveHistory extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      leaveRequest: []
+      leaveHistory: []
     };
   }
   loadHistoryData() {
@@ -24,22 +30,63 @@ class StaffLeaveHistory extends Component {
       url: API_BASE_URL + "/appliedleave/me/" + thisYear,
       method: "GET"
     })
-      .then(data => this.setState({ leaveRequest: data }))
+      .then(data => this.setState({ leaveHistory: data }))
       .catch(err => {
         // if unable to fetch data, assign default (spaces) to values
-        let leaveRequest = [];
-        this.setState({ leaveRequest: leaveRequest });
+        let leaveHistory = [];
+        this.setState({ leaveHistory: leaveHistory });
       });
   }
   componentDidMount() {
     this.loadHistoryData();
   }
   render() {
-    const { leaveRequest } = this.state;
-
     if (!isManagerRole(this.props.currentUser)) {
       return <Redirect to="/forbidden" />;
     }
+
+    const leaveHistoryCols = [
+      {
+        id: "name",
+        Header: "Employee Name",
+        accessor: "employeeDetails.name",
+        minWidth: 140,
+        sortable: true,
+        filterable: true
+      },
+      {
+        id: "leaveType",
+        Header: "Leave Type",
+        accessor: "leaveCategory.leaveDescr",
+        minWidth: 140,
+        sortable: true,
+        filterable: true
+      },
+      {
+        id: "leaveStatus",
+        Header: "Status",
+        accessor: "leaveStatus",
+        minWidth: 140,
+        sortable: true,
+        filterable: true
+      },
+      {
+        id: "startDate",
+        Header: "Start Date",
+        accessor: d => formatDateDMY(d.id.startDate),
+        minWidth: 94,
+        sortable: true,
+        filterable: true
+      },
+      {
+        id: "endDate",
+        Header: "End Date",
+        accessor: d => formatDateDMY(d.endDate),
+        minWidth: 94,
+        sortable: true,
+        filterable: true
+      }
+    ];
 
     return (
       <div className="mainContainerFlex">
@@ -49,7 +96,23 @@ class StaffLeaveHistory extends Component {
           </span>
         </div>
         <br />
-        <div className="tableContainerFlex">
+        <div className="reactTableContainer">
+          <ReactTable
+            data={this.state.leaveHistory}
+            columns={leaveHistoryCols}
+            defaultPageSize={10}
+            pages={this.state.pages}
+            loading={this.state.loading}
+            filterable={true}
+            sortable={true}
+            multiSort={true}
+            // rowsText="Rows per page"
+            loadingText="Loading Leave History..."
+            noDataText="No data available."
+            className="-striped"
+          />
+          </div>
+        {/* <div className="tableContainerFlex">
           <Table responsive>
             <thead>
               <tr>
@@ -78,7 +141,7 @@ class StaffLeaveHistory extends Component {
               })}
             </tbody>
           </Table>
-        </div>
+        </div> */}
       </div>
     );
   }
