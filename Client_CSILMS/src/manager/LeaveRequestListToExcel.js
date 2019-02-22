70fc8597ea6c7a3d1de3794c48eea95d03fc092a
flindralin <cherry.marvel@gmail.com>
2019-02-22 15:20:34 +0800

    added export to excel button for (leave request list & leave category) pages

--- Client_CSILMS/src/manager/LeaveRequestListToExcel.js
+++ Client_CSILMS/src/manager/LeaveRequestListToExcel.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ReactHTMLTableToExcel from "react-html-table-to-excel";
+import { formatDateDMY } from "../util/APIUtils";
 import "../common/Styles.css";
 
 class ExportToExcel extends Component {
@@ -31,10 +32,16 @@ class ExportToExcel extends Component {
                     <tbody>
                         {this.props.leaveRequestData.map(LeaveRequest => {
                             return (
-                                <tr key={LeaveRequest.leaveCode}>
-                                    <td>{LeaveRequest.leaveCode}</td>
-                                    <td>{LeaveRequest.leaveDescr}</td>
-                                    <td>{LeaveRequest.entitlement}</td>
+                                <tr key={LeaveRequest.id.emplid}>
+                                    <td>{LeaveRequest.id.emplid}</td>
+                                    <td>{LeaveRequest.employeeDetails.name}</td>
+                                    <td>{LeaveRequest.employeeDetails.jobTitle}</td>
+                                    <td>{formatDateDMY(LeaveRequest.id.startDate)}</td>
+                                    <td>{formatDateDMY(LeaveRequest.endDate)}</td>
+                                    <td>{LeaveRequest.leaveCategory.leaveDescr}</td>
+                                    <td>{LeaveRequest.halfDay}</td>
+                                    <td>{LeaveRequest.leaveDuration}</td>
+                                    <td>{LeaveRequest.leaveStatus}</td>
                                 </tr>
                             );
                         })}
