8a6e10d71e12d78a85bdd681014ef3a89ab1f0aa
khairulpadzli <43407258+khairulpadzli@users.noreply.github.com>
2019-02-25 17:17:30 +0800

    handle locked account

--- Server_CSILMS/src/main/java/com/csi/leavemanagement/controllers/TranslateitemRestController.java
+++ Server_CSILMS/src/main/java/com/csi/leavemanagement/controllers/TranslateitemRestController.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.csi.leavemanagement.models.JobDetails;
 import com.csi.leavemanagement.models.Translateitem;
-import com.csi.leavemanagement.models.TranslateitemId;
 import com.csi.leavemanagement.services.TranslateitemService;
 
 @RestController
