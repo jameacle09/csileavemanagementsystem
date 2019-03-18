5fd1507c9b76f651de2d037b6fcc76f1732e6a12
acapawn7 <acapawn7@gmail.com>
2019-03-18 15:32:26 +0800

    Added max length for leave code in Add Leave Category

--- Client_CSILMS/src/hradmin/EditLeaveCategory.js
+++ Client_CSILMS/src/hradmin/EditLeaveCategory.js
@@ -161,6 +161,7 @@ class EditLeaveCategory extends Component {
                   id="leaveCode"
                   value={leaveCode}
                   onChange={this.handleChange}
+                  maxLength="3"
                   required
                   disabled
                 />
