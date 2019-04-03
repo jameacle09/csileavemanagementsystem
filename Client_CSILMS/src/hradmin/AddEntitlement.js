d823317dfecdbc64fae1e47f7e362698c6498075
acapawn7 <acapawn7@gmail.com>
2019-04-03 22:03:56 +0800

    2 digits for Available Leave in Leave Entitlements

--- Client_CSILMS/src/hradmin/AddEntitlement.js
+++ Client_CSILMS/src/hradmin/AddEntitlement.js
@@ -132,6 +132,7 @@ class AddEntitlement extends Component {
       isNaN(entitlement) ||
       availableLeave === "" ||
       availableLeave < 0 ||
+      isNaN(availableLeave) ||
       takenLeave === "" ||
       balanceLeave === "";
     return isInvalid;
@@ -300,14 +301,14 @@ class AddEntitlement extends Component {
       )
 
     let availableLeaveMessage = "";
-    if(availableLeave < 0 )
+    if(isNaN(availableLeave) || availableLeave < 0 )
       availableLeaveMessage = (
         <Alert 
           color="danger" 
           xs={4} sm={3} 
           style={{padding: ".25em 1em", margin: ".25em 1em"}} 
         > 
-        Positive number only 
+        Please enter a valid number only 
         </Alert>
       )
 
@@ -432,7 +433,8 @@ class AddEntitlement extends Component {
               </Label>
               <Col xs={4} sm={2}>
                 <Input
-                  type="number"
+                  type="text"
+                  maxLength="2"
                   name="availableLeave"
                   id="availableLeave"
                   placeholder="Available Leave"
