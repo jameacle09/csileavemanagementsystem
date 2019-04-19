11946a1b62e2717074780a8060065bf126a8cab6
netkiko <netkiko@gmail.com>
2019-04-19 15:57:32 +0800

    Merged

--- Client_CSILMS/src/util/APIUtils.js
+++ Client_CSILMS/src/util/APIUtils.js
@@ -243,14 +243,9 @@ export function exportTableToExcel(tableID, filename) {
   var tableSelect = document.getElementById(tableID);
   var tableHTML = tableSelect.outerHTML.replace(/ /g, "%20");
 
-  // Specify file name
   filename = filename ? filename + ".xls" : "excel_data.xls";
-
-  // Create download link element
   downloadLink = document.createElement("a");
-
   document.body.appendChild(downloadLink);
-
   if (navigator.msSaveOrOpenBlob) {
     const table = document.getElementById(tableID).outerHTML;
     const fileData = [
@@ -263,18 +258,9 @@ export function exportTableToExcel(tableID, filename) {
     ];
     const blobObject = new Blob(fileData);
     navigator.msSaveOrOpenBlob(blobObject, filename);
-    // var blob = new Blob(["\ufeff", tableHTML], {
-    //   type: dataType
-    // });
-    // navigator.msSaveOrOpenBlob(blob, filename);
   } else {
-    // Create a link to the file
     downloadLink.href = "data:" + dataType + ", " + tableHTML;
-
-    // Setting the file name
     downloadLink.download = filename;
-
-    //triggering the function
     downloadLink.click();
   }
 }
